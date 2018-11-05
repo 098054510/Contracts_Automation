a2919e37a076ace6a33d3c552d7c244d7b77d700
Eduardo de Souza <eduardohsfreitas86@gmail.com>
2018-11-05 08:36:20 -0300

    First Progress sum.

--- Contracs_Automation/Program.cs
+++ Contracs_Automation/Program.cs
@@ -13,35 +13,26 @@ namespace Contracs_Automation
     {
         static void Main(string[] args)
         {
-            Console.Write("How much Contracts will be registered: ");
-            int N = int.Parse(Console.ReadLine());
-            for (int i = 1; 1 <= N; i++)
-            {
-                Console.WriteLine("Enter Contract Data:");
-                Console.Write("Number: ");
-                int Number = int.Parse(Console.ReadLine());
-                Console.Write("Date (dd/MM/yyyy): ");
-                DateTime D = DateTime.Parse(Console.ReadLine());
-                Console.Write("Contract Value: $");
-                double ContractValue = double.Parse(Console.ReadLine());
-                Console.Write("Enter the number of installments: ");
-                int Mounth = int.Parse(Console.ReadLine());
-                Contract contract = new Contract(Mounth, ContractValue);
-                PayPal paypal = new PayPal(contract);
-                paypal.CValue = ContractValue / Mounth;
-
-                Console.WriteLine("$" + paypal.CValue.ToString("F2", CultureInfo.InvariantCulture));
-
-                for (int ii = 0; ii < Mounth; ii++)
-                {
-                    double Sub, Final, Final2;
-                    Sub = 1.01 + paypal.CValue * Mounth;
-                    Final = 2 % +Sub;
-                    Final2 = Final + Sub;
-                    Console.WriteLine("$" + Final2.ToString("F2", CultureInfo.InvariantCulture));
-                }
-                Console.WriteLine(D);
-            }
+            Console.WriteLine("Enter Contract Data:");
+            Console.Write("Number: ");
+            int Number = int.Parse(Console.ReadLine());
+            Console.Write("Date (dd/MM/yyyy): ");
+            DateTime D = DateTime.Parse(Console.ReadLine());
+            Console.Write("Contract Value: $");
+            double ContractValue = double.Parse(Console.ReadLine());
+            Console.Write("Enter the number of installments: ");
+            int Mounth = int.Parse(Console.ReadLine());
+            Contract contract = new Contract(Mounth, ContractValue);
+            PayPal paypal = new PayPal(contract);
+            paypal.CValue = ContractValue / Mounth;
+            Console.WriteLine("$" + paypal.CValue.ToString("F2", CultureInfo.InvariantCulture));
+            double Percent, Final, Final2;
+            Percent = paypal.CValue / 100;
+            Final = paypal.CValue + (3 / 100);
+            Final2 = Final + Percent;
+            Console.WriteLine("$" + Percent.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("$" + Final2.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(D);
             Console.ReadLine();
         }
     }
